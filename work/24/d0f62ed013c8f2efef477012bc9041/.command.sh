65a1ebfb0d7373bad39531d6042cd9d5c0e8d6d2
fwa010 <frans.wallin@regionorebrolan.se>
2023-05-04 15:58:10 +0200

    Added base for pipeline and a emu module

--- work/24/d0f62ed013c8f2efef477012bc9041/.command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -euo pipefail
-check_samplesheet.py \
-    samplesheet_mod.csv \
-    samplesheet.valid.csv
-
-cat <<-END_VERSIONS > versions.yml
-"NFCORE_GMSEMU:GMSEMU:INPUT_CHECK:SAMPLESHEET_CHECK":
-    python: $(python --version | sed 's/Python //g')
-END_VERSIONS
