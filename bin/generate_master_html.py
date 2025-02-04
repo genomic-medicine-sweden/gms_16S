c7c77f730c398942722f27247d859b4bd88465f6
ryanjameskennedy <ryanjameskennedy@icloud.com>
2025-02-04 09:52:06 +0100

    Update generate_master_html to include timestap as input variable

--- bin/generate_master_html.py
+++ bin/generate_master_html.py
@@ -66,6 +66,13 @@ parser.add_argument(
     dest='html',
     required=True
     )
+parser.add_argument(
+    '-t', '--timestamp',
+    help='pipeline execution timestamp',
+    metavar='PIPELINE_EXECUTION_TIMESTAMP',
+    dest='timestamp',
+    required=True
+    )
 
 args = parser.parse_args()
 
@@ -89,18 +96,18 @@ def get_sample_ids(samplesheet_csv):
     sample_ids = df['sample'].tolist()
     return sample_ids
 
-def generate_master_html(template_html_fpath, sample_ids, seqrun_date):
+def generate_master_html(template_html_fpath, sample_ids, seqrun_date, timestamp):
     """Read the template from an HTML file."""
     with open(template_html_fpath, "r") as file:
         master_template = file.read()
     template = Template(master_template)
-    rendered_html = template.render(sample_ids=sample_ids, seqrun_date=seqrun_date)
+    rendered_html = template.render(sample_ids=sample_ids, seqrun_date=seqrun_date, timestamp=timestamp)
     return rendered_html
 
 def main():
     sample_ids = get_sample_ids(args.csv)
     seqrun_date = find_date_in_string(args.csv, r'/(\d{8})_')
-    rendered_html = generate_master_html(args.html, sample_ids, seqrun_date)
+    rendered_html = generate_master_html(args.html, sample_ids, seqrun_date, args.timestamp)
     with open("master.html", "w") as fout:
         fout.write(rendered_html)
 
